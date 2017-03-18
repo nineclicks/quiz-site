c0684c07dbbe0d0a571da9935568d35479225b05
Nick <nicholasg1989@gmail.com>
2017-03-18 13:18:42 -0700

    Cats have 30 teeth (12 incisors, 10 premolars, 4 canines, and 4 molars), while dogs have 42. Kittens have baby teeth, which are replaced by permanent teeth around the age of 7 months.

--- src/clj/quiz_site/routes/quiz.clj
+++ src/clj/quiz_site/routes/quiz.clj
@@ -12,6 +12,30 @@
   (log/debug "args:   " args)
   {:error "Implementation for this method not found."})
 
+(defn get-quiz-results
+  [& {:keys[quiz-id] :as args}]
+  (log/debug "get-quiz")
+  (log/debug "args:   " args)
+  {:error "Implementation for this method not found."})
+
+(defn create-quiz
+  [& {:keys[quiz-id] :as args}]
+  (log/debug "create-quiz")
+  (log/debug "args:   " args)
+  {:error "Implementation for this method not found."})
+
+(defn submit-answers
+  [& {:keys[quiz-id] :as args}]
+  (log/debug "submit-answers")
+  (log/debug "args:   " args)
+  {:error "Implementation for this method not found."})
+
 (def quiz-routes
-  (context "/quiz" []
-           (GET  "/:quiz-id" {params :params} (wrap-status (mapply get-quiz params)))))
+  (defroutes document-routes
+    (context "/quiz" []
+             (POST "/" {params :params} (wrap-status (mapply create-quiz params)))
+             (context "/:quiz-id" [quiz-id]
+                      (GET  "/" {params :params} (wrap-status (mapply get-quiz       params)))
+                      (POST "/" {params :params} (wrap-status (mapply submit-answers params)))))
+    (context "/results/:results-id" [results-id]
+             (GET "/" {params :params} (wrap-status (mapply get-quiz-results params))))))
